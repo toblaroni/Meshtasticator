c29713c3d54f50cdd93bf49999f0c71c54d5dcf4
medentem <info@medentem.com>
2025-01-03 14:12:32 -0600

    more logging

--- loraMesh.py
+++ loraMesh.py
@@ -242,9 +242,9 @@ class MeshNode():
 						if rebroadcastProbabilityTest <= rebroadcastProbability:
 							self.packets.append(pNew)
 							self.env.process(self.transmit(pNew))
-							verboseprint('At time', round(self.env.now, 3), 'node', self.nodeid, 'rebroadcasts received packet', p.seq, '. New Coverage: ', newCoverageCount)
+							verboseprint('At time', round(self.env.now, 3), 'node', self.nodeid, 'rebroadcasts received packet', p.seq, '. New Coverage: ', newCoverageCount, 'Rnd: ', rebroadcastProbabilityTest, 'Prob: ', rebroadcastProbability)
 						else:
-							verboseprint('At time', round(self.env.now, 3), 'node', self.nodeid, 'drops received packet due to coverage', p.seq, '. New Coverage: ', newCoverageCount)
+							verboseprint('At time', round(self.env.now, 3), 'node', self.nodeid, 'drops received packet due to coverage', p.seq, '. New Coverage: ', newCoverageCount, 'Rnd: ', rebroadcastProbabilityTest, 'Prob: ', rebroadcastProbability)
 
 
 if VERBOSE:
